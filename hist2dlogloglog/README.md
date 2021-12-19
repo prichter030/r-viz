b2588d79c87c64486856207bd37c50ce1c61ff72
Philipp Richter <richterp@csail.nit.edu>
2021-12-19 16:40:19 +0100

    hist2dlogloglog added

---
+++ hist2dlogloglog/README.md
@@ -0,0 +1,39 @@
+# hist2dlogloglog
+
+2-dimensional histogram with support logarithmic axes. Best suited for large, heavily skewed datasets.
+
+
+## Installation / Dependencies
+
+### Dependencies
+```r
+install.packages("gplots") # contains original hist2d, which hist2dplus is based on.
+install.packages("RColorBrewer") # required for beautiful colors.
+install.packages("fields") # required to plot z-axis legend.
+install.packages("scales") # required for log-scale axis computations.
+```
+### Load hist2dplus
+```r
+source("hist2dplus.R")
+
+```
+
+### Example
+
+```r
+# generarte heavily skewed data, e.g., from a Pareto distribution. 
+
+library(Pareto)
+
+x = rPareto(10000,1000,0.1)
+y = rPareto(10000,1000,0.1)
+
+# summary(x)
+#      Min.   1st Qu.    Median      Mean   3rd Qu.      Max. 
+# 1.002e+03 1.816e+04 1.021e+06 3.674e+34 8.414e+08 2.958e+38 
+# (yep, that data is heavily skewed)
+
+hist2dlogloglog(x,y,log="xyz",xlab="X values" ,ylab="Y values", main="hist2dlogloglog example")
+```
+
+![hist2dlogloglog example showing Pareto distribution of X and Y](hist2dlogloglog_paretoex.png)
---
+++ hist2dlogloglog/hist2dlogloglog.R
@@ -0,0 +1,88 @@
+library(gplots)
+library(RColorBrewer)
+library(fields)
+library(scales)
+
+
+hist2dlogloglog <- function(datax, datay,nbins=100,col=rf(32),log="",...){
+  par(mar=c(6,6,6,7))
+  
+  # parse where logarithmic axes are desired (x,y, and z as color-code)
+  if(grepl("x",log,fixed = T)){
+    logx = T
+  }else{
+    logx = F
+  }
+  if(grepl("y",log,fixed=T)){
+    logy=T
+  }else{
+    logy = F
+  }
+  if(grepl("z",log,fixed = T)){
+    logz = T
+  }else{
+    logz = F
+  }
+  
+  rf <- colorRampPalette(rev(brewer.pal(11,'Spectral')))
+  
+  #check if logging X axis is desired
+  
+  if(logx){
+    datax = log(datax)
+  }
+  
+  if(logy){
+    datay = log(datay)
+  }
+  
+  h <- hist2d(datax, datay,nbins=nbins,show=F)
+  
+  if(logz){
+    h$counts = log(h$counts+1)
+  }
+  h$counts[h$counts == 0] <- NA
+  
+  #h_log_counts <- log(h$counts+1)
+  #h_log_counts[h_log_counts == 0]<- NA
+  
+  image(x=h$x, y= h$y, z=h$counts,col=col,axes=F,...)
+  print(h$x)
+  print(log_breaks(base=10)(exp(h$x)))
+  
+  if(logx){
+    xticks = log(log_breaks(base=10)(exp(h$x)))
+    xlabels = log_breaks(base=10)(exp(h$x))  
+  }else{
+    xticks = pretty(h$x)
+    xlabels = pretty(h$x)
+  }
+  
+  if(logy){
+    yticks = log(log_breaks(base=10)(exp(h$y)))
+    ylabels = log_breaks(base=10)(exp(h$y))
+  }else{
+    yticks = pretty(h$y)
+    ylabels = pretty(h$y)
+  }
+  
+  
+  #allow overplotting so that all labels get placed
+  par(xpd=T)
+  axis(1,at=xticks,labels=xlabels)
+  axis(2,at=yticks,labels=ylabels)
+  par(xpd=F)
+  
+  
+  #plot grid
+  abline(v=xticks,col="grey",lty=3)
+  abline(h=yticks,col="grey",lty=3)
+
+  if(logz){
+  image.plot(h$counts,legend.only = T,col = col, legend.line=2.9,axis.args = list(at=log(log_breaks(n=5,base=10)(exp(h$counts))), labels=log_breaks(n=5,base=10)(exp(h$counts))))
+  }else{
+    image.plot(h$counts,legend.only = T,col = col, legend.line=2.9)
+    
+  }
+}
+
